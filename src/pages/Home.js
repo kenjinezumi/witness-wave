7139f01fcfcf5127b1e2acec8aae0228c2815de3
kenjinezumi <kenjitsuchiya1@gmail.com>
2024-09-14 22:50:56 +0100

    Change the styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,30 +4,52 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
 const Home = () => {
-  const getOptions = () => ({
-    title: {
-      text: 'Statistics of Police Violence',
-    },
-    tooltip: {},
-    xAxis: {
-      data: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
-    },
-    yAxis: {},
-    series: [{
-      name: 'Incidents',
-      type: 'bar',
-      data: [120, 200, 150, 80, 70, 110, 130],
-    }],
-  });
+    const getOptions = () => ({
+        backgroundColor: '#1e1e1e',
+        textStyle: {
+          color: '#f5f5f5',
+        },
+        title: {
+          text: 'Police Violence Statistics',
+          left: 'center',
+          textStyle: {
+            color: '#ffffff',
+          },
+        },
+        xAxis: {
+          type: 'category',
+          data: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+          axisLine: { lineStyle: { color: '#f5f5f5' } },
+          axisLabel: { color: '#f5f5f5' },
+        },
+        yAxis: {
+          type: 'value',
+          axisLine: { lineStyle: { color: '#f5f5f5' } },
+          axisLabel: { color: '#f5f5f5' },
+        },
+        series: [
+          {
+            name: 'Incidents',
+            type: 'bar',
+            data: [120, 200, 150, 80, 70, 110, 130],
+            itemStyle: {
+              color: '#e63946',
+            },
+          },
+        ],
+      });
+      
 
   return (
     <div>
       {/* Map Section */}
       <div style={{ height: '400px', marginBottom: '20px' }}>
         <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: '100%', width: '100%' }}>
-          <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
+        <TileLayer
+  url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+/>
+
           <Marker position={[51.505, -0.09]}>
             <Popup>
               Police Violence Incident <br /> Date: 2023-09-10.
