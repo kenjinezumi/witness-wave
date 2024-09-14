2a12157967f1407eba3c6ca702a545d32e1e42ee
kenjinezumi <kenjitsuchiya1@gmail.com>
2024-09-15 00:40:18 +0100

    add logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../Header.css'; // Import the CSS file for styling
+import '../Header.css'; 
+import logo from '../assets/logo.png'; 
 
 const Header = () => {
   return (
     <header className="header">
       <nav className="navbar">
+      <div className="logo">
+            <img src={logo} alt="WitnessWave Logo" className="logo-img" />
+        </div>
         <div className="logo">
+        
           <Link to="/">WitnessWave</Link> {/* You can change this text */}
         </div>
         <ul className="nav-links">
