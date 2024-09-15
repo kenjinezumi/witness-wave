27aab8c1d0d9a8ca4d2f3851a3249fb9a483d7f9
kenjinezumi <kenjitsuchiya1@gmail.com>
2024-09-15 01:04:41 +0100

    Small change to the map

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -132,11 +132,21 @@ const Home = () => {
 
       {/* Map Section */}
       <div className="map-section">
-        <MapContainer center={mapCenter} zoom={13} style={{ height: '100%', width: '100%' }}>
-          <TileLayer
-            url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-          />
+      <MapContainer 
+  center={mapCenter} 
+  minZoom={3} 
+  maxZoom={8} 
+  zoom={5}  // Set default zoom level
+  style={{ height: '100%', width: '100%' }}
+  scrollWheelZoom={false}  // Optionally disable zooming with scroll
+>
+        <TileLayer
+  url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+  minZoom={3}
+  maxZoom={8}
+/>
+
           <Marker position={mapCenter}>
             <Popup>
               Police Violence Incident <br /> Date: 2023-09-10.
